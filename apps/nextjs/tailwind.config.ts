92a77ba5c5958eb65b2cc3334225bbfc53466eac
atng <tng.weiheng.alvin@gmail.com>
2023-08-01 20:44:05 +0800

    update

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -3,6 +3,6 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@acme/tailwind-config";
 
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}", "../../packages/ui/components/*.{ts,tsx}"],
   presets: [baseConfig],
 } satisfies Config;
